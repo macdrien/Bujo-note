d95482c54ed49591190af566b8821494fd3a4d54
macdrien <adrien.bouyssou@gmail.com>
2024-05-30 08:10:32 +0200

    ADD Initial commit with default Spring project

---
+++ backend/src/main/kotlin/fr/sidranie/bujo_note/BujoNoteApplication.kt
@@ -0,0 +1,11 @@
+package fr.sidranie.bujo_note
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BujoNoteApplication
+
+fun main(args: Array<String>) {
+	runApplication<BujoNoteApplication>(*args)
+}
---
+++ backend/src/test/kotlin/fr/sidranie/bujo_note/BujoNoteApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.sidranie.bujo_note
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BujoNoteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
